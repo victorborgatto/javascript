ce3d4e752528730faedb3c03b376e9352c26b909
VIctor Borgatto <162215748+victorborgatto@users.noreply.github.com>
2024-04-09 23:55:25 -0300

    updt

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(`A velocidade do seu  carro é ${vel}Km/h`)
+if (vel > 60) { //condição simples!
+    console.log(`Você ultrpassou a velocidade permitida. Multado!`)
+
+}
+console.log(` dirija sempre usanso cinto de cigurança!`)
