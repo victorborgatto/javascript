c2fbc36023a7b4bd1997b020d21b8bc834eb752f
VIctor Borgatto <162215748+victorborgatto@users.noreply.github.com>
2024-04-15 10:23:43 -0300

    up

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+var c = 54
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 6)
+
+/*var c = 1
+while(c < 500) {
+    console.log(`Passo ${c}`)
+    c++
+} */
+/*console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')*/
